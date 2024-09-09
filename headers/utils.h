1a6c35309929014485b8c440cca8d7fc67e9a564
Sumisgard <ibyrsik@gmail.com>
2024-09-09 12:02:19 +0300

    Changed file structure + wrote setters

--- headers/utils.h
+++ headers/utils.h
@@ -9,7 +9,7 @@ namespace utils
     private:
         class Vehicle
         {
-        protected:
+        private:
             std::string brand;
             std::string model;
         public:
@@ -17,21 +17,73 @@ namespace utils
             {
                 this->brand = brand;
             }
+            void set_model(std::string brand)
+            {
+                this->brand = brand;
+            }
         };
 
-        class Car
+        class Car: public Vehicle
         {
-
+        private:
+            unsigned int engineVolume;
+            std::string color;
+            std::string checkpointType;
+        public:
+            void set_engineVolume(unsigned int liters)
+            {
+                this->engineVolume = liters;
+            }
+            void set_color(std::string color)
+            {
+                this->color = color;
+            }
+            void set_checkpointType(std::string type)
+            {
+                this->checkpointType = type;
+            }
         };
 
-        class Motorcycle
+        class Motorcycle: public Vehicle
         {
-
+        private:
+            unsigned int engineVolume;
+            unsigned int horsepower;
+            std::string preferredTerrain;
+        public:
+            void set_engineVolume(unsigned int liters)
+            {
+                this->engineVolume = liters;
+            }
+            void set_horsepower(unsigned int power)
+            {
+                this->horsepower = power;
+            }
+            void set_preferredTerrain(std::string terrain)
+            {
+                this->preferredTerrain = terrain;
+            }
         };
 
-        class Bus
+        class Bus: public Vehicle
         {
-
+        private:
+            unsigned int passengerSeats;
+            unsigned int totalSeats;
+            std::string destination;
+        public:
+            void set_passengerSeats(unsigned int seats)
+            {
+                this->passengerSeats = seats;
+            }
+            void set_totalSeats(unsigned int seats)
+            {
+                this->totalSeats = seats;
+            }
+            void set_destination(std::string destination)
+            {
+                this->destination = destination;
+            }           
         };
 
         Car* cars;
