91a5cb6ec211d9fe8135fdcfd7497732928b1ed8
Sumisgard <ibyrsik@gmail.com>
2024-09-17 23:39:40 +0300

    Started writing main function

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include "utils.hpp"
 
+using namespace utils;
 
 int main(void)
 {
-    
+    Garage container;
+
+    container.addCar("BMW", "X5", 5, "gay blue", "In");
+    container.addCar("Audi", "Q5", 5, "lesbian pink", "Out");
+
+    container.addMotorcycle("Yamaha", "");
+
     return 0;
 }
